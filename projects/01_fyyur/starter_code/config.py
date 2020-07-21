ce229cf37ea8f2470d28c281101afe0492999cde
Aya Kenawy <ayahkenawy4490@gmail.com>
2020-07-21 23:45:50 +0200

    README REQUIRMENTS

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,4 +1,7 @@
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +10,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+app = Flask(__name__)
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234@localhost:5432/fyyur'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+db=SQLAlchemy(app)
+migrate = Migrate(app, db)
